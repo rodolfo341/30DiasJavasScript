395207de8d493923867e56106c3ede269ee2b780
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-09-07 21:38:06 -0300

    desafio 30 dias javascript dia 2

--- dia_2/dia_2.js
+++ dia_2/dia_2.js
@@ -125,15 +125,17 @@ console.log(desafio.repeat(2))
 // 1    Usando console.log() imprima la siguiente declaración:
 //      The quote 'There is no exercise better for the heart than reaching down and lifting people up.' 
 //      by John Holmes teaches us to help one another.
-console.log(`}
-The quote 'There is no exercise better for the heart than reaching down and lifting people up.
-' by John Holmes teaches us to help one another.`);
-
+console.log(`The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.`);
 
 // 2    Usando console.log() imprima la siguiente cita de la Madre Teresa:
 //      "Love is not patronizing and charity isn't about pity, it is about love. 
 //      Charity and love are the same -- with charity you give love, 
 //      so don't just give money but reach out your hand instead."
+console.log(`"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."`)
+
+
+
+
 // 3    Compruebe si typeof '10' es exactamente igual a 10. Si no, hágalo exactamente igual.
 // 4    Compruebe si parseFloat('9.8') es igual a 10, si no, hágalo exactamente igual a 10.
 // 5    Verifique si 'on' se encuentra tanto en Python como en la jerga
