ca869b369d0ff9211eba972ab63f895602febb6f
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-09-07 21:30:27 -0300

    desafio 30 dias javascript dia 2

--- dia_2/dia_2.js
+++ dia_2/dia_2.js
@@ -129,6 +129,7 @@ console.log(`}
 The quote 'There is no exercise better for the heart than reaching down and lifting people up.
 ' by John Holmes teaches us to help one another.`);
 
+
 // 2    Usando console.log() imprima la siguiente cita de la Madre Teresa:
 //      "Love is not patronizing and charity isn't about pity, it is about love. 
 //      Charity and love are the same -- with charity you give love, 
