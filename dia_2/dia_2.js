d9c2f92f914f1c2ddee6ddf44f698b9c0c31d39a
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-09-19 14:50:46 -0300

    desafio 30 dias javascript dia 2

--- dia_2/dia_2.js
+++ dia_2/dia_2.js
@@ -196,12 +196,18 @@ for (let j = 0; j<=50;j++) {
     console.log(`${cadena[i]}`);
 }
 
-
 // 11   Use console.log() y caracteres de escape para imprimir el siguiente patrón.
-
-
+/*
+    1 1 1 1 1
+    2 1 2 4 8
+    3 1 3 9 27
+    4 1 4 16 64
+    5 1 5 25 125
+*/
+console.log(`1 1 1 1 1\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125`);
 
 //   Divide la cadena en un array usando el método split()
+
 //   Divida la cadena 30 días de JavaScript en el espacio usando el método split()
 //   'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' divide la cadena en la coma y cámbiala a una matriz.
 //   Cambie 30 días de JavaScript a 30 días de Python usando el método replace().
