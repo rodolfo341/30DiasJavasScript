f00fd9c16d66cc6bbe83415754db011211cbf225
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-09-19 22:46:16 -0300

    desafio 30 dias javascript dia 3

--- dia_3/dia_3.js
+++ dia_3/dia_3.js
@@ -9,7 +9,21 @@
 //          isMarried, 
 //          year y 
 //          asignar un valor, use el operador typeof para verificar diferentes tipos de datos.
+let firstName = "Rodolfo"; 
+let lastName = "Aravena"; 
+let country = "Chile"; 
+let city = "Santiago"; 
+let age = 51; 
+let isMarried = true; 
+let year = 2023;
 
+console.log(typeof firstName);
+console.log(typeof lastName);
+console.log(typeof country);
+console.log(typeof city);
+console.log(typeof age);
+console.log(typeof isMarried);
+console.log(typeof year);
 
 
 /*
