2c7ab6338b4e0d6a95ac257af9f773d287b21bf8
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-09-19 22:40:57 -0300

    desafio 30 dias javascript dia 3

--- dia_3/dia_3.js
+++ dia_3/dia_3.js
@@ -1,1 +1,131 @@
-//
+// Ejercicios: Nivel 1
+
+// 1    Declare las siguientes variables; 
+//          firstName, 
+//          lastName, 
+//          country, 
+//          city, 
+//          age, 
+//          isMarried, 
+//          year y 
+//          asignar un valor, use el operador typeof para verificar diferentes tipos de datos.
+
+
+
+/*
+    Verifique si typeof '10' es igual a 10
+
+    Verifique si parseInt('9.8') es igual a 10
+
+    Verifique cualquier valor booleano true o false.
+        Escriba tres declaraciones de JavaScript que proporcionen un valor verdadero.
+        Escriba tres declaraciones de JavaScript que proporcionen un valor falso.
+
+    Calcule primero el resultado de la siguiente expresión de comparación sin usar console.log(). Después de decidir el resultado, confirmelo usando console.log()
+        4 > 3
+        4 >= 3
+        4 < 3
+        4 <= 3
+        4 == 4
+        4 === 4
+        4 != 4
+        4 !== 4
+        4 != '4'
+        4 == '4'
+        4 === '4'
+        Encuentre la longitud de Python y jargon y haga una declaración de comparación falsa.
+
+    Calcule primero el resultado de las siguientes expresiones sin usar console.log(). Después de decidir el resultado, confirmelo usando console.log()
+        4 > 3 && 10 < 12
+        4 > 3 && 10 > 12
+        4 > 3 || 10 < 12
+        4 > 3 || 10 > 12
+        !(4 > 3)
+        !(4 < 3)
+        !(false)
+        !(4 > 3 && 10 < 12)
+        !(4 > 3 && 10 > 12)
+        !(4 === '4')
+        No hay 'on' tanto en dragon como en python
+
+    Utilice el objeto Date para realizar las siguientes actividades
+        ¿Qué año es hoy?
+        ¿Qué mes es hoy con un número?
+        ¿Qué fecha es hoy?
+        ¿Qué día es hoy con un número?
+        ¿Cuál es la hora actual?
+        ¿Cuántos minutos hay actualmente?
+        Averigüe el número de segundos transcurridos desde el 1 de enero de 1970 hasta ahora.
+
+Ejercicios: Nivel 2
+
+    Escriba un script que solicite al usuario que ingrese la base y la altura del triángulo y calcule el área de un triángulo (área = 0,5 x b x h).
+
+    Ingrese base: 20
+    Ingrese altura: 10
+    El área del triángulo es: 100
+
+Escriba un script que solicite al usuario que ingrese el lado a, el lado b y el lado c del triángulo y calcule el perímetro del triángulo (perímetro = a + b + c)
+
+Ingrese lado a: 5
+Ingrese lado b: 4
+Ingrese lado c: 3
+El perimetro del triangulo es: 12
+
+Obtenga el largo y el ancho usando prompt y calcule el área del rectángulo (área = largo x ancho y el perímetro del rectángulo (perímetro = 2 x (largo + ancho))
+
+Obtenga el radio usando prompt y calcule el área de un círculo (área = pi x r x r) y la circunferencia de un círculo (c = 2 x pi x r) donde pi = 3.14.
+
+Calcule la pendiente, la intersección X y la intersección Y de y = 2x -2
+
+La pendiente es m = (y2-y1)/(x2-x1). Encuentra la pendiente entre el punto (2, 2) y el punto (6,10)
+
+Compare la pendiente de las dos preguntas anteriores.
+
+Calcula el valor de y (y = x2 + 6x + 9). Trate de usar diferentes valores de x y averigüe en qué valor de x y es 0.
+
+Escriba un script con prompt que solicite al usuario que ingrese las horas y la tarifa por hora. ¿Calcular el salario de la persona?
+
+Ingrese horas: 40
+Introduce la tarifa por hora: 28
+Su ganancia semanal es 1120
+
+Si la longitud de su nombre es mayor que 7, diga que su nombre es largo; de lo contrario, diga que su nombre es corto.
+
+Compare la longitud de su nombre y la longitud de su apellido y debería obtener este resultado.
+
+let firstName = "Asabeneh";
+let lastName = "Yetayeh";
+
+Tu primer nombre, Asabeneh, es más largo que tu apellido, Yetayeh.
+
+Declare dos variables myAge y yourAge y asignarles los valores iniciales y myAge y yourAge.
+
+let myAge = 250;
+let yourAge = 25;
+
+Soy 225 años mayor que tú.
+
+Usando prompt, obtenga el año en que nació el usuario y, si el usuario tiene 18 años o más, permita que el usuario conduzca, si no dígale que espere una cierta cantidad de años.
+
+Introduzca el año de nacimiento: 1995
+Tienes 25 años. Tienes la edad suficiente para conducir.
+
+Introduzca el año de nacimiento: 2005
+Tienes 15 años. Podrás conducir después de 3 años.
+
+Escriba un script que solicite por prompt al usuario que ingrese el número de años. Calcular el número de segundos que puede vivir una persona. Supongamos que alguien vive solo cien años
+
+Ingrese el número de años de vida: 100
+ Viviste 3153600000 segundos.
+
+    Cree un formato de hora legible por humanos usando el objeto Date.
+        YYYY-MM-DD HH:mm
+        DD-MM-YYYY HH:mm
+        DD/MM/YYYY HH:mm
+
+Ejercicios: Nivel 3
+
+    Cree un formato de hora legible por humanos usando el objeto Date. La hora y el minuto deben ser siempre dos dígitos (7 horas deben ser 07 y 5 minutos deben ser 05)
+        YYY-MM-DD HH:mm eg. 20120-01-02 07:05
+*/
